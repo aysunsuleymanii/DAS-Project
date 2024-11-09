d502dd93ca8246954de23e9efed7bf2d2c0c7cc4
aysunsuleymanii <aysunsuleymani402@gmail.com>
2024-11-09 22:00:18 +0100

    Added stock_data.xlsx

--- Homework1/csv_to_excel.py
+++ Homework1/csv_to_excel.py
@@ -1,9 +1,8 @@
 import pandas as pd
 
-# Load the CSV file into a DataFrame
-csv_file_path = "stock_data.csv"
+csv_file_path = "/Users/aysunsuleymanturk/Desktop/Software Arch and Design/SDA-Project/Homework1/stock_data.csv"
+
 data = pd.read_csv(csv_file_path)
 
-# Save the DataFrame to an Excel file
 excel_file_path = "stock_data.xlsx"
 data.to_excel(excel_file_path, index=False)
