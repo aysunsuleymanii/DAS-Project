de09d4fbd6b25b7590b5a8b020345e6fd92c630a
Aysun Suleymani <129193517+aysunsuleymanii@users.noreply.github.com>
2024-11-09 22:06:49 +0100

    Update csv_to_excel.py

--- Homework1/csv_to_excel.py
+++ Homework1/csv_to_excel.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-csv_file_path = "/Users/aysunsuleymanturk/Desktop/Software Arch and Design/SDA-Project/Homework1/stock_data.csv"
+csv_file_path = "stock_data.csv"
 
 data = pd.read_csv(csv_file_path)
 
