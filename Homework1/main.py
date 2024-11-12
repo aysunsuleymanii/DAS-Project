a2b43b6ee770bf5421daa47e82369fd97490e6fe
aysunsuleymanii <aysunsuleymani402@gmail.com>
2024-11-12 22:25:21 +0100

    Add new files for Homework 1

--- Homework1/main.py
+++ Homework1/main.py
@@ -11,18 +11,43 @@ BASE_URL = 'https://www.mse.mk/en/stats/symbolhistory/'
 CSV_FILE = 'stock_data.csv'
 
 
-# Filter 1: Extract Issuers
+# Filter 1: Extract Issuers with filtering based on condition
 async def extract_issuers(session):
-    async with session.get(BASE_URL + 'ALK') as response:
-        soup = BeautifulSoup(await response.text(), 'html.parser')
-        select_element = soup.find('select', id='Code')
-        if not select_element:
-            print("The company is not found")
+    url = 'https://www.mse.mk/en/stats/current-schedule'
+
+    async with session.get(url) as response:
+        if response.status != 200:
+            print("Failed to fetch the webpage.")
             return []
 
-        companies = select_element.find_all('option')
-        return [option.get('value') for option in companies if
-                not (option.get('value')[0] in 'EMS' or re.search(r'\d', option.get('value')))]
+        soup = BeautifulSoup(await response.text(), 'html.parser')
+
+        tab_ids = [
+            'results-continuousTradingMode',
+            'results-fixingWith20PercentLimit',
+            'results-fixingWithoutLimit'
+        ]
+
+        issuers = []
+
+        for tab_id in tab_ids:
+            tab_content = soup.find('div', {'id': tab_id})
+
+            if tab_content:
+                table = tab_content.find('table')
+
+                if table:
+                    rows = table.find_all('tr')
+
+                    for row in rows:
+                        symbol_cell = row.find('a', href=True)
+                        if symbol_cell:
+                            issuer_name = symbol_cell.text.strip()
+
+                            if not (issuer_name[0] in 'EMS' or re.search(r'\d', issuer_name)):
+                                issuers.append(issuer_name)
+
+        return issuers
 
 
 # Filter 2: Check Last Available Date
@@ -135,6 +160,8 @@ async def main():
 
     async with aiohttp.ClientSession() as session:
         issuers = await extract_issuers(session)
+        print(f"Filtered Issuers: {issuers}")
+
         issuer_dates = check_last_date(issuers)
         all_data = []
 
